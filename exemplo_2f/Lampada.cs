630da01201fab2209f519a3faf0abd35d4120466
Anderson Vanin <53703505+ProfAndersonVanin@users.noreply.github.com>
2023-02-15 16:53:41 -0300

    Exemplo da Classe Lampada em C#

---
+++ exemplo_2f/Lampada.cs
@@ -0,0 +1,17 @@
+namespace exemplo;
+class Lampada
+{
+    public bool ligada;
+    public double potencia;
+
+    public void ligar(){
+        ligada = true;
+    }
+    public void desligar(){
+        ligada = false;
+    }
+
+    public bool estaLigada(){
+        return ligada;
+    }
+} 
